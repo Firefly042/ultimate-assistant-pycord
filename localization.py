7c3141b9be4ea16703d5595d1f84d235d6779421
Firefly042 <17179785+Firefly042@users.noreply.github.com>
2022-05-24 09:40:12 -0400

    contribution test

---
+++ localization.py
@@ -0,0 +1,78 @@
+strings = {
+    "common": {
+        "no-character": {
+            "en-US": "You do not have an active character in this server! Do you need to use `/profile swap`?",
+            "it": "<it>"
+        },
+        "visible-desc": {
+            "en-US": "Set to 'True' for a permanent, visible response.",
+            "it": "<it>"
+        },
+        "visible-name": {
+            "en-US": "visible",
+            "it": "<it>"
+        }
+    },
+
+    "profile": {
+        "view": {
+            "name": {
+                "en-US": "view",
+                "it": "<it>"
+            },
+            "description": {
+                "en-US": "View a character's profile",
+                "it": "<it>"
+            },
+            "options": {
+                "player": {
+                    "name": {
+                        "en-US": "name",
+                        "it": "<it>"
+                    },
+                    "description": {
+                        "en-US": "The user who plays this character",
+                        "it": "<it>"
+                    }
+                },
+                "name": {
+                    "name": {
+                        "en-US": "name",
+                        "it": "<it>"
+                    },
+                    "description": {
+                        "en-US": "Character's display name (usually given name)",
+                        "it": "<it>"
+                    }
+                }
+            },
+            "responses": {
+                "error1": {
+                    "en-US": "Cannot find that character for that player!",
+                    "it": "<it>"
+                }
+            }
+        },
+
+        "list": {
+            "name": {
+                "en-US": "list",
+                "it": "<it>"
+            },
+            "description": {
+                "en-US": "List all registered characters for this server",
+                "it": "<it>"
+            },
+            "responses": {
+                "res1": {
+                    "en-US": "Characters in {}",
+                    "it": "<it>"
+                },
+                "error1": {
+                    "en-US": "This server has no registered characters!",
+                    "it": "<it>"
+                }
+            }
+        }
+    }
+}
