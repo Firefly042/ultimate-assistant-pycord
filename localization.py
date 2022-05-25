3f9c4c7876398fbf82fa98971d680529c58b0f92
Firefly042 <17179785+Firefly042@users.noreply.github.com>
2022-05-25 15:46:47 -0400

    indentation fix

--- localization.py
+++ localization.py
@@ -1,126 +1,128 @@
 """String localization"""
 
 strings = {
-    "common": {
-	"no-character": {
-	    "en-US": "You do not have an active character in this server! Do you need to use `/profile scambio`?",
-	    "it": "Non hai un personaggio attivo in questo server! Hai bisogno di usare `/profile scambio`?"
-	},
-	"visible-desc": {
-	    "en-US": "Set to 'True' for a permanent, visible response.",
-	    "it": "Imposta a 'True' per una risposta visibile e permanente."
-	},
-	"visible-name": {
-	    "en-US": "visible",
-	    "it": "visibile"
-	}
-    },
-
-    "profile": {
-	"group_name": {
-	    "en-US": "profile",
-	    "it": "profilo"
-	},
-	"group_description": {
-	    "en-US": "Character profiles"
-	},
-
-	"commands": {
-	    "view": {
-		"name": {
-		    "en-US": "view",
-		    "it": "visualizza"
+	"common": {
+		"no-character": {
+			"en-US": "You do not have an active character in this server! Do you need to use `/profile scambio`?",
+			"it": "Non hai un personaggio attivo in questo server! Hai bisogno di usare `/profile scambio`?"
 		},
-		"description": {
-		    "en-US": "View a character's profile",
-		    "it": "Visualizza il profilo di un personaggio"
+		"visible-desc": {
+			"en-US": "Set to 'True' for a permanent, visible response.",
+			"it": "Imposta a 'True' per una risposta visibile e permanente."
 		},
-		"options": {
-		    "player": {
-			"name": {
-			    "en-US": "player",
-			    "it": "giocatore"
-			},
-			"description": {
-			    "en-US": "The user who plays this character",
-			    "it": "L'utente che usa questo personaggio"
-			}
-		    },
-		    "name": {
-			"name": {
-			    "en-US": "name",
-			    "it": "nome"
-			},
-			"description": {
-			    "en-US": "Character's display name (usually given name)",
-			    "it": "Il nome da display del personaggio"
-			}
-		    }
-		},
-		"responses": {
-		    "error1": {
-			"en-US": "Cannot find that character for that player!",
-			"it": "Non riesco a trovare quel personaggio per quel giocatore!"
-		    }
+		"visible-name": {
+			"en-US": "visible",
+			"it": "visibile"
 		}
-	    },
+	},
 
-	    "list": {
-		"name": {
-		    "en-US": "list",
-		    "it": "lista"
+	"profile": {
+		"group_name": {
+			"en-US": "profile",
+			"it": "profilo"
 		},
-		"description": {
-		    "en-US": "List all registered characters for this server",
-		    "it": "Lista tutti i personaggi registrati per questo server"
+		"group_description": {
+			"en-US": "Character profiles"
 		},
-		"responses": {
-		    "res1": {
-			"en-US": "Characters in {}",
-			"it": "Personaggi in {}"
-		    },
-		    "error1": {
-			"en-US": "This server has no registered characters!",
-			"it": "Questo server non ha personaggi registrati!"
-		    }
+
+		"commands": {
+			"view": {
+				"name": {
+					"en-US": "view",
+					"it": "visualizza"
+				},
+				"description": {
+					"en-US": "View a character's profile",
+					"it": "Visualizza il profilo di un personaggio"
+				},
+				"options": {
+					"player": {
+						"name": {
+							"en-US": "player",
+							"it": "giocatore"
+						},
+						"description": {
+							"en-US": "The user who plays this character",
+							"it": "L'utente che usa questo personaggio"
+						}
+					},
+					"name": {
+						"name": {
+							"en-US": "name",
+							"it": "nome"
+						},
+						"description": {
+							"en-US": "Character's display name (usually given name)",
+							"it": "Il nome da display del personaggio"
+						}
+					}
+				},
+				"responses": {
+					"error1": {
+						"en-US": "Cannot find that character for that player!",
+						"it": "Non riesco a trovare quel personaggio per quel giocatore!"
+					}
+				}
+			},
+
+			"list": {
+				"name": {
+					"en-US": "list",
+					"it": "lista"
+				},
+				"description": {
+					"en-US": "List all registered characters for this server",
+					"it": "Lista tutti i personaggi registrati per questo server"
+				},
+				"responses": {
+					"res1": {
+						"en-US": "Characters in {}",
+						"it": "Personaggi in {}"
+					},
+					"error1": {
+						"en-US": "This server has no registered characters!",
+						"it": "Questo server non ha personaggi registrati!"
+					}
+				}
+			},
+
+			""
 		}
-	    }
 	}
-    }
 }
 
 
 def group_names(group):
-    return strings[group]["group_name"]
+	return strings[group]["group_name"]
 
 
 def group_descriptions(group):
-    return strings[group]["group_descriptions"]
+	return strings[group]["group_description"]
 
 
 def command_names(group, command):
-    return strings[group]["commands"][command]["name"]
+	return strings[group]["commands"][command]["name"]
 
 
 def command_descriptions(group, command):
-    return strings[group]["commands"][command]["description"]
+	return strings[group]["commands"][command]["description"]
 
 
 def option_names(group, command, option):
-    return strings[group]["commands"][command]["options"][option]["name"]
+	return strings[group]["commands"][command]["options"][option]["name"]
 
 
 def option_descriptions(group, command, option):
-    return strings[group]["commands"][command]["options"][option]["description"]
+	return strings[group]["commands"][command]["options"][option]["description"]
 
 
 def response(group, command, res_tag, locale):
-    """Requires locale parameter (ctx.interaction.locale)"""
+	"""Requires locale parameter (ctx.interaction.locale)"""
 
-    res = strings[group]["commands"][command]["responses"][res_tag][locale]
+	res = strings[group]["commands"][command]["responses"][res_tag][locale]
 
-    if (not res):
+	if (not res):
 	return strings[group]["commands"][command]["responses"][res_tag]["en-US"]
 
-    return res
+	return res
 
