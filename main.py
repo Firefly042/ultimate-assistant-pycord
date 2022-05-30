d6730eb7dca1eb6aaff0b372cec1a8c20b424e3b
Firefly042 <17179785+Firefly042@users.noreply.github.com>
2022-05-30 09:55:38 -0400

    Removed message content

--- main.py
+++ main.py
@@ -12,19 +12,17 @@ import discord
 from config import TESTING_SERVERS
 import db
 
-HELP_MESSAGE = "If you are seeing this, Ultimate Assistant has officially migrated to slash commands! Try typing `/` into discord and checking them out.\n\nIf you do not see them, you may need to re-invite the bot by clicking its name and using the in-profile invite. After that, the slash commands may take an hour or so to register to your server.\n<https://discord.com/api/oauth2/authorize?client_id=668970741525381141&permissions=274877975552&scope=bot%20applications.commands>\n\nTo get started, type `/help`!\n\n(This command will be phased out by 31st August, 2022)"
 
 # ------------------------------------------------------------------------
 # Adjust bot intents
 # ------------------------------------------------------------------------
 bot_intents = discord.Intents.default()
-bot_intents.message_content = True
 
 
 # ------------------------------------------------------------------------
 # Instantiate bot
 # ------------------------------------------------------------------------
-bot = discord.Bot(debug_guilds=TESTING_SERVERS, intents=bot_intents, activity=discord.Game(name="!help (IMPORTANT CHANGES)"))
+bot = discord.Bot(debug_guilds=TESTING_SERVERS, intents=bot_intents, activity=discord.Game(name="CHECK PROFILE DESCRIPTION"))
 
 
 # ------------------------------------------------------------------------
@@ -64,14 +62,6 @@ async def on_ready():
 	print(f"Logged in as {bot.user}")
 
 
-@bot.event
-async def on_message(message):
-	"""A temporary help command for transition period"""
-	if (message.content.lower() == "!help"):
-		try:
-			await message.channel.send(HELP_MESSAGE)
-		except discord.Forbidden:
-			return
 # ------------------------------------------------------------------------
 # Cog handling
 # ------------------------------------------------------------------------
