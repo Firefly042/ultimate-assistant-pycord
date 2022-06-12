bb8a9c660cbb0dffc1054e17d2bb9e8d32ae6813
Firefly042 <17179785+Firefly042@users.noreply.github.com>
2022-06-12 09:44:29 -0400

    comment out sqlite db backup

--- cogs/default_cog.py
+++ cogs/default_cog.py
@@ -40,24 +40,24 @@ class DefaultCog(commands.Cog):
 # Crontabs appear to execute in a LIFO stack order
 # Do not need to be explicitly started
 # ------------------------------------------------------------------------
-	@staticmethod
-	@aiocron.crontab('0 1 * * *')
-	async def backup_db():
-		"""
-		Backs up the database once every 24 hours at 1AM, server time
-		Files older than 30 days will be removed
-		"""
+	# @staticmethod
+	# @aiocron.crontab('0 1 * * *')
+	# async def backup_db():
+	# 	"""
+	# 	Backs up the database once every 24 hours at 1AM, server time
+	# 	Files older than 30 days will be removed
+	# 	"""
 
-		today = datetime.date.today()
-		fname = f"./db_backups/{today}.db"
-		shutil.copy("main.db", fname)
+	# 	today = datetime.date.today()
+	# 	fname = f"./db_backups/{today}.db"
+	# 	shutil.copy("main.db", fname)
 
-		for file in os.listdir("./db_backups"):
-			fpath = os.path.join("./db_backups", file)
-			mod_date = datetime.datetime.fromtimestamp(os.path.getmtime(fpath))
+	# 	for file in os.listdir("./db_backups"):
+	# 		fpath = os.path.join("./db_backups", file)
+	# 		mod_date = datetime.datetime.fromtimestamp(os.path.getmtime(fpath))
 
-			if (file.endswith(".db") and datetime.datetime.now() - mod_date > datetime.timedelta(days=30)):
-				os.remove(fpath)
+	# 		if (file.endswith(".db") and datetime.datetime.now() - mod_date > datetime.timedelta(days=30)):
+	# 			os.remove(fpath)
 
 
 # ------------------------------------------------------------------------
